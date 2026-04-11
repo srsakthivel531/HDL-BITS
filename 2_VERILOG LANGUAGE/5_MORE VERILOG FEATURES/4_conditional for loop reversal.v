a25576dca26a6ea8e67d59d64d415252b3239e65
Sakthivel S <srsakthivel531@gmail.com>
2026-04-11 18:57:24 +0530

    Update 4_conditional for loop reversal.v

--- 2_VERILOG LANGUAGE/5_MORE VERILOG FEATURES/4_conditional for loop reversal.v
+++ 2_VERILOG LANGUAGE/5_MORE VERILOG FEATURES/4_conditional for loop reversal.v
@@ -1,10 +1,10 @@
 module top_module( 
     input [99:0] in,
-    output [99:0] out
-);
+    output reg [99:0] out);
     integer i;
-    always @(*) begin
-        for(i=99;i>=0;i--)
+    always@(*)begin
+        for(i=0;i<=99;i=i+1)begin
             out[i]=in[99-i];
-    end
+        end 
+    end 
 endmodule
