5a1ac100e2bcbada10cce52145afc673b75d7814
Sakthivel S <srsakthivel531@gmail.com>
2026-04-11 18:58:37 +0530

    Create 5_for_loop_population count.v

---
+++ 2_VERILOG LANGUAGE/5_MORE VERILOG FEATURES/5_for_loop_population count.v
@@ -0,0 +1,14 @@
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+integer i;
+reg [7:0]count;
+always@(*)begin
+    count=0;
+    for(i=0;i<255;i=i+1)begin
+        if(in[i]==1)
+           count=count+1;
+    end 
+end
+assign out=count;
+endmodule
