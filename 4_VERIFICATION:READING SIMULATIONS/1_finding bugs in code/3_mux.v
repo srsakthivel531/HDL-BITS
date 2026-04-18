20460e5d9b86a7b3ec27355525e49f57956809d6
Sakthivel S <srsakthivel531@gmail.com>
2026-04-18 23:47:25 +0530

    Create 3_mux.v

---
+++ 4_VERIFICATION:READING SIMULATIONS/1_finding bugs in code/3_mux.v
@@ -0,0 +1,30 @@
+//WITH BUG IN THE CODE 
+module top_module (
+    input [1:0] sel,
+    input [7:0] a,
+    input [7:0] b,
+    input [7:0] c,
+    input [7:0] d,
+    output [7:0] out  ); //
+
+    wire mux0, mux1;
+    mux2 mux0 ( sel[0],    a,    b, mux0 );
+    mux2 mux1 ( sel[1],    c,    d, mux1 );
+    mux2 mux2 ( sel[1], mux0, mux1,  out );
+
+endmodule
+//AFTER BUG REMOVAL CODE 
+module top_module (
+    input [1:0] sel,
+    input [7:0] a,
+    input [7:0] b,
+    input [7:0] c,
+    input [7:0] d,
+    output [7:0] out  ); //
+
+    wire [7:0]mux0, mux1;
+    mux2 mu0 ( sel[0],    a,    b, mux0 );
+    mux2 mu1 ( sel[0],    c,    d, mux1 );
+    mux2 mu2 ( sel[1], mux0, mux1,  out );
+
+endmodule
