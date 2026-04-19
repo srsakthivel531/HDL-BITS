7419ab1a1441d71359959691b5151d0df51cda3e
Sakthivel S <srsakthivel531@gmail.com>
2026-04-19 22:25:37 +0530

    Create 09_sequencial ckt9.v

---
+++ 4_VERIFICATION:READING SIMULATIONS/2_Build a circuit from a simulation waveform/09_sequencial ckt9.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input a,
+    output [3:0] q );
+    always@(posedge clk)begin
+        if(a)
+            q<=4'h4;
+    else if(q==4'h6)
+        q<=4'h0;
+     else 
+        q<=q+1;
+    end 
+endmodule
